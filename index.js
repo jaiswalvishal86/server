f667362f1f73b70abc7ac1b7994c3a146984dd24
Vishal Jaiswal <vishal@Vishals-MacBook-Air.local>
2024-05-30 17:23:04 +0530

    add popup route

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cors());
 
 app.post("/api/submit-form", async (req, res) => {
   const data = req.body;
-  const formName = req.get("Referer").split("/").pop();
+
   try {
     let organizationId = null;
     let personId = null;
@@ -100,6 +100,66 @@ app.post("/api/submit-form", async (req, res) => {
     res
       .status(500)
       .json({ message: "Error processing form data", error: error.message });
+  }
+});
+
+app.post("/api/popup-form", async (req, res) => {
+  const data = req.body;
+
+  let organizationId = null;
+  let personId = null;
+
+  const orgResponse = await axios.post(
+    `https://api.pipedrive.com/v1/organizations`,
+    { name: "UPDOT" },
+    { params: { api_token: apiToken } }
+  );
+  const orgData = orgResponse.data;
+  if (orgData.success) {
+    organizationId = orgData.data.id;
+  }
+
+  if (organizationId) {
+    const personData = {
+      name: data.name,
+      email: [{ value: data.email, primary: true }],
+      phone: data.phone,
+      org_id: organizationId,
+    };
+
+    const personResponse = await axios.post(
+      `https://api.pipedrive.com/v1/persons`,
+      personData,
+      { params: { api_token: apiToken } }
+    );
+    const personJsonData = personResponse.data;
+    if (personJsonData.success) {
+      personId = personJsonData.data.id;
+    }
+  }
+
+  if (personId && organizationId) {
+    const leadData = {
+      title: `New Lead from ${data.formName}`,
+      person_id: personId,
+      organization_id: organizationId,
+      "856d510f94582b0d7d7ce7b22628d5287c8d0e6a": data.code,
+      "05ffd2968a78543c2c6092c73ca718f915281197": data.message,
+    };
+
+    const leadResponse = await axios.post(
+      `https://api.pipedrive.com/v1/leads`,
+      leadData,
+      { params: { api_token: apiToken } }
+    );
+    const leadResponseData = leadResponse.data;
+    if (leadResponseData.success) {
+      res.status(200).json({ message: "Form submitted successfully!" });
+    } else {
+      res
+        .status(500)
+        .json({ message: "Failed to submit form.", error: leadResponseData });
+    }
   }
 });
 
