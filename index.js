7a2cadc17d542d48d76f8c438be5caba35f14262
Vishal Jaiswal <vishal@Vishals-MacBook-Air.local>
2024-05-30 15:06:46 +0530

    server

---
+++ index.js
@@ -0,0 +1,110 @@
+import dotenv from "dotenv";
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+import cors from "cors";
+// import serverless from "serverless-http";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const apiToken = process.env.API_TOKEN;
+
+if (!apiToken) {
+  console.error(
+    "API token is missing. Please set the API_TOKEN environment variable."
+  );
+  process.exit(1);
+}
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.post("/api/submit-form", async (req, res) => {
+  const data = req.body;
+  try {
+    let organizationId = null;
+    let personId = null;
+
+    if (data.company.length >= 2) {
+      const searchResponse = await axios.get(
+        `https://api.pipedrive.com/v1/organizations/search`,
+        {
+          params: { term: data.company, api_token: apiToken },
+        }
+      );
+      const searchData = searchResponse.data;
+      if (searchData.success && searchData.data.items.length > 0) {
+        organizationId = searchData.data.items[0].item.id;
+      } else {
+        const orgResponse = await axios.post(
+          `https://api.pipedrive.com/v1/organizations`,
+          { name: data.company },
+          { params: { api_token: apiToken } }
+        );
+        const orgData = orgResponse.data;
+        if (orgData.success) {
+          organizationId = orgData.data.id;
+        }
+      }
+    }
+
+    if (organizationId) {
+      const personData = {
+        name: data.name,
+        email: [{ value: data.email, primary: true }],
+        phone: data.phone,
+        org_id: organizationId,
+      };
+
+      const personResponse = await axios.post(
+        `https://api.pipedrive.com/v1/persons`,
+        personData,
+        { params: { api_token: apiToken } }
+      );
+      const personJsonData = personResponse.data;
+      if (personJsonData.success) {
+        personId = personJsonData.data.id;
+      }
+    }
+
+    // Create lead
+    if (personId && organizationId) {
+      const leadData = {
+        title: "New Lead Webflow Hiring Form",
+        person_id: personId,
+        organization_id: organizationId,
+        "856d510f94582b0d7d7ce7b22628d5287c8d0e6a": data.code,
+        "5fd6a4e3043b2f7083183f47c8b3e7ffd71c0c9d": data.designation,
+      };
+
+      const leadResponse = await axios.post(
+        `https://api.pipedrive.com/v1/leads`,
+        leadData,
+        { params: { api_token: apiToken } }
+      );
+      const leadResponseData = leadResponse.data;
+      if (leadResponseData.success) {
+        res.status(200).json({ message: "Form submitted successfully!" });
+      } else {
+        res
+          .status(500)
+          .json({ message: "Failed to submit form.", error: leadResponseData });
+      }
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error processing form data", error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// export { app };
+// export const handler = serverless(app);
